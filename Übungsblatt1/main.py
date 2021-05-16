dd5efdc4780e3d5c4e6eb9b1b6cdec52f8cdc178
dominimvllasa <do571vll@htwg-konstanz.de>
2021-05-16 17:47:10 +0200

    Explantation of numpy-functions

--- Übungsblatt1/main.py
+++ Übungsblatt1/main.py
@@ -1,27 +1,34 @@
-import math
-
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy import stats
 import argparse
 import json
-
 from sklearn.linear_model import LinearRegression
 
 """
-Aufgabe 1.2
+Übungsblatt1
 """
+
+
+def convert_param_to_list(urliste):
+    urliste = json.loads(urliste)
+    urliste = np.array(urliste)
+    return urliste
+
+
 def plot_diagramms(urliste):
     # x = np.array([1,2,3,4,5])
     # y = np.array([3,1,3,1,2])
     # y2 = np.array([3,4,7,8,10])
-    urliste = json.loads(urliste)
-    x, y = np.unique(urliste, return_counts=True)
+    urliste = convert_param_to_list(urliste)
 
-    y2 = np.cumsum(y)
-    d = np.array(urliste)
-    d = np.sort(d)
-    print(d)
+    x, y = np.unique(urliste, return_counts=True) # [1,1,2,2,1,5]  --> x= [1,2,3,4,5]; y= [3,2,1]
+
+    y2 = np.cumsum(y) # Cumulated list [2,3,5,2] --> [2,5,10,12]
+
+    sorted_urliste = np.sort(urliste) # Sort list [2,2,1,1,4,] --> [1,1,2,2,4]
+    print("Sortierte Urliste: \n",
+          sorted_urliste)
 
     """
         Plot Histogram
@@ -37,72 +44,52 @@ def plot_diagramms(urliste):
 
 
 def characteristics_of_sample(urliste):
-    urliste = json.loads(urliste)
-    urliste = np.array(urliste)
-    a = np.mean(urliste)
-    b = np.median(urliste)
-    c = stats.mode(urliste)
-    d = np.var(urliste, ddof=1)
-    e = np.std(urliste, ddof=1)
-    q75, q25 = np.percentile(urliste, [75, 25])
-    q66, q30 = np.percentile(urliste, [66, 30])
-    q90 = np.percentile(urliste, 90)
-    q10 = np.percentile(urliste, 10)
-    iqr = q75 - q25
+    urliste = convert_param_to_list(urliste)
+
+    a = np.mean(urliste) # Mittelwert
+    b = np.median(urliste) # Median
+    c = stats.mode(urliste) # Modalwert
+    d = np.var(urliste, ddof=1) # Varianz
+    e = np.std(urliste, ddof=1) # Standardabweichung
+    q75, q25 = np.percentile(urliste, [75, 25]) # Quantile
+    q66, q30 = np.percentile(urliste, [66, 30]) # Quantile
+    q90 = np.percentile(urliste, 90) # Quantile
+    q10 = np.percentile(urliste, 10) # Quantile
+    iqr = q75 - q25 # Interquartilabstand
     smax = np.max(urliste)
     smin = np.min(urliste)
-    spannweite = smax - smin
+    spannweite = smax - smin # Spannweite
 
-    print("### Befehl - Arithmetisches Mittel: urliste = json.loads(urliste);  urliste = np.array(urliste);  arithmetisches_mittel = np.mean(urliste) ### \n",
-          "Arithmetisches Mittel: ", a, "\n\n",
-
-          "### Befehl - Media: urliste = json.loads(urliste);  urliste = np.array(urliste);  Median = np.median(urliste) ### \n"
+    print("Arithmetisches Mittel: ", a, "\n\n",
           "Median               : ", b, "\n\n",
-
-          "### Befehl - Modalwert: urliste = json.loads(urliste);  urliste = np.array(urliste);  Modalwert = np.mode(urliste) ### \n"
           "Modalwert            : ", c, "\n\n",
-          "### Befehl - Varianz: urliste = json.loads(urliste);  urliste = np.array(urliste);  varianz = np.var(urliste, ddof=1) ###  \n"
-
           "Varianz              : ", d, "\n\n",
-          "### Befehl - Standardabweichung: urliste = json.loads(urliste);  urliste = np.array(urliste);  Standarabweichung = np.std(urliste, ddof=1) ### \n"
           "Standardabweichung   : ", e, "\n\n",
-
-          "### Befehl - 90% Quantil: urliste = json.loads(urliste);  urliste = np.array(urliste);  Quantil_90 = np.std(urliste, ddof=1) ### \n"
           "90% Quantil          : ", q90, "\n\n",
-
-          "### Befehl - 75% Quantil: urliste = json.loads(urliste);  urliste = np.array(urliste);  Quantil_75 = np.std(urliste, ddof=1) ### \n"
           "75% Quantil          : ", q75, "\n\n",
-
-          "### Befehl - 66% Quantil: urliste = json.loads(urliste);  urliste = np.array(urliste);  Quantil_66 = np.std(urliste, ddof=1) ### \n"
           "66% Quantil          : ", q66, "\n\n",
-
-          "### Befehl - 30% Quantil: urliste = json.loads(urliste);  urliste = np.array(urliste);  Quantil_30 = np.std(urliste, ddof=1) ### \n"
           "30% Quantil          : ", q30, "\n\n",
-
-          "### Befehl - 25% Quantil: urliste = json.loads(urliste);  urliste = np.array(urliste);  Quantil_25 = np.std(urliste, ddof=1) ###  \n"
           "25% Quantil          : ", q25, "\n\n",
-
-          "### Befehl - 10% Quantil: urliste = json.loads(urliste);  urliste = np.array(urliste);  Quantil_10 = np.std(urliste, ddof=1) ### \n"
           "10% Quantil          : ", q10, "\n\n",
-
-          "### Befehl - Interquartilabstand: urliste = Interquartilabstand = q75-q25 ### \n"
           "Interquartilabstand  : ", iqr, "\n\n",
-
-          "### Befehl - Spannweite: smax = np.max(urliste); smin = np.min(urliste); spannweite = smax - smin ### \n"
           "Spannweite           : ", spannweite, "\n\n", )
 
 
-
 def calculate_coefficient(cof1, cof2):
-    cof1 = json.loads(cof1)
-    cof1 = np.array(cof1)
-    cof2 = json.loads(cof2)
-    cof2 = np.array(cof2)
+    cof1 = convert_param_to_list(cof1)
+    cof2 = convert_param_to_list(cof2)
 
-    print(np.corrcoef(cof1,cof2)[0,1])
+    print("Korrelationskoeffizient: \n",
+          np.corrcoef(cof1,cof2)[0,1])
 
+    """
+        Punktediagramm
+    """
     plt.plot(cof1, cof2, 'bo')
 
+    """
+        Regressionsgerade
+    """
     cof1 = cof1.reshape(-1, 1)
     cof2 = cof2.reshape(-1, 1)
 
@@ -110,9 +97,7 @@ def calculate_coefficient(cof1, cof2):
     linear_regressor.fit(cof1, cof2)  # perform linear regression
     Y_pred = linear_regressor.predict(cof1)  # make predictions
     plt.plot(cof1, Y_pred, color='red')
-
-
-    # plt.show()
+    plt.show()
 
 
 if __name__ == '__main__':
