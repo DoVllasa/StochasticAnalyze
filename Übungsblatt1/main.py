5b03f692b60157e95e139a8d3d1927369ebda171
dominimvllasa <do571vll@htwg-konstanz.de>
2021-05-16 17:28:19 +0200

    Calcualtion Corelationcoefficient

--- Übungsblatt1/main.py
+++ Übungsblatt1/main.py
@@ -1,8 +1,12 @@
+import math
+
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy import stats
 import argparse
 import json
+
+from sklearn.linear_model import LinearRegression
 
 """
 Aufgabe 1.2
@@ -41,6 +45,8 @@ def characteristics_of_sample(urliste):
     d = np.var(urliste, ddof=1)
     e = np.std(urliste, ddof=1)
     q75, q25 = np.percentile(urliste, [75, 25])
+    q66, q30 = np.percentile(urliste, [66, 30])
+    q90 = np.percentile(urliste, 90)
     q10 = np.percentile(urliste, 10)
     iqr = q75 - q25
     smax = np.max(urliste)
@@ -61,8 +67,17 @@ def characteristics_of_sample(urliste):
           "### Befehl - Standardabweichung: urliste = json.loads(urliste);  urliste = np.array(urliste);  Standarabweichung = np.std(urliste, ddof=1) ### \n"
           "Standardabweichung   : ", e, "\n\n",
 
+          "### Befehl - 90% Quantil: urliste = json.loads(urliste);  urliste = np.array(urliste);  Quantil_90 = np.std(urliste, ddof=1) ### \n"
+          "90% Quantil          : ", q90, "\n\n",
+
           "### Befehl - 75% Quantil: urliste = json.loads(urliste);  urliste = np.array(urliste);  Quantil_75 = np.std(urliste, ddof=1) ### \n"
           "75% Quantil          : ", q75, "\n\n",
+
+          "### Befehl - 66% Quantil: urliste = json.loads(urliste);  urliste = np.array(urliste);  Quantil_66 = np.std(urliste, ddof=1) ### \n"
+          "66% Quantil          : ", q66, "\n\n",
+
+          "### Befehl - 30% Quantil: urliste = json.loads(urliste);  urliste = np.array(urliste);  Quantil_30 = np.std(urliste, ddof=1) ### \n"
+          "30% Quantil          : ", q30, "\n\n",
 
           "### Befehl - 25% Quantil: urliste = json.loads(urliste);  urliste = np.array(urliste);  Quantil_25 = np.std(urliste, ddof=1) ###  \n"
           "25% Quantil          : ", q25, "\n\n",
@@ -77,14 +92,43 @@ def characteristics_of_sample(urliste):
           "Spannweite           : ", spannweite, "\n\n", )
 
 
+
+def calculate_coefficient(cof1, cof2):
+    cof1 = json.loads(cof1)
+    cof1 = np.array(cof1)
+    cof2 = json.loads(cof2)
+    cof2 = np.array(cof2)
+
+    print(np.corrcoef(cof1,cof2)[0,1])
+
+    plt.plot(cof1, cof2, 'bo')
+
+    cof1 = cof1.reshape(-1, 1)
+    cof2 = cof2.reshape(-1, 1)
+
+    linear_regressor = LinearRegression()
+    linear_regressor.fit(cof1, cof2)  # perform linear regression
+    Y_pred = linear_regressor.predict(cof1)  # make predictions
+    plt.plot(cof1, Y_pred, color='red')
+
+
+    # plt.show()
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--urliste")
+    parser.add_argument("--cof1")
+    parser.add_argument("--cof2")
     parser.add_argument("--plot", type=bool)
     args = parser.parse_args()
     urliste = args.urliste
+    cof1 = args.cof1
+    cof2 = args.cof2
     plot = args.plot
     if urliste:
         characteristics_of_sample(urliste)
     if plot == True:
         plot_diagramms(urliste)
+    if cof1 and cof2:
+        calculate_coefficient(cof1,cof2)
