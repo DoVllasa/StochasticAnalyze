2bd90d9d67194330a26d0cb4a63cbda5a1a1528a
dominimvllasa <do571vll@htwg-konstanz.de>
2021-05-26 01:12:19 +0200

    Added Variation, Combination and Permutation

--- Übungsblatt1/main.py
+++ Übungsblatt1/main.py
@@ -4,9 +4,58 @@ from scipy import stats
 import argparse
 import json
 from sklearn.linear_model import LinearRegression
+import math
 
 """
-Übungsblatt1
+    Übungsblatt2
+"""
+
+class Uebungsblatt2():
+
+    def __init__(self, n, k, repetition=False):
+            self.n = int(n)
+            self.k = int(k)
+            self.repetition = repetition
+
+    def combinatoric(self):
+        """
+            Matematical combinatoric:
+            C(n,k) = n! / k!*(n-k)! --> Without repetition # 1
+            C'(n,k) = (n + k -1)! / (n - 1)! * k! --> With repitition # 2
+        """
+
+        if self.repetition:
+            comb_result = math.factorial(self.n + self.k -1) / math.factorial(self.n - 1) * math.factorial(self.k)  # 2
+            print("C'(n,k) = (n + k -1)! / (n - 1)! * k! --> With repitition: ", comb_result)
+        else:
+            comb_result = math.factorial(self.n) / math.factorial(self.k) * math.factorial(self.n - self.k) # 1
+            print("C(n,k) = n! / k!*(n-k)! --> Without repetition: ", comb_result)
+
+    def variation(self):
+        """
+            Mathematical variation:
+            V(n,k) = n!/(n-k)! --> without repitition # 1
+            V'(n,k) = n^k # 2 --> With repitition # 2
+        """
+
+        if self.repetition:
+            var_result = self.n ** self.k # 2
+            print('V(n,k) = n!/(n-k)! --> without repitition: ', var_result)
+        else:
+            var_result = math.factorial(self.n) / math.factorial(self.n - self.k) # 1
+            print("V'(n,k) = n^k --> With repitition: ", var_result)
+
+    def permutation(self):
+        """
+            Mathematical permutation:
+            P(n) = n!
+        """
+        permutation_result = math.factorial(self.n)
+        print(permutation_result)
+
+
+"""
+    Übungsblatt1
 """
 
 class Uebungsblatt1():
@@ -112,16 +161,64 @@ if __name__ == '__main__':
     parser.add_argument("--cof1")
     parser.add_argument("--cof2")
     parser.add_argument("--plot", type=bool)
+    parser.add_argument("--n")
+    parser.add_argument("--k")
+    parser.add_argument('--rep', action='store_true')
+    parser.add_argument('--no-rep', action='store_false')
+    parser.add_argument("--comb", action='store_true')
+    parser.add_argument("--var", action='store_true')
+    parser.add_argument("--per", action='store_true')
+
     args = parser.parse_args()
     urliste = args.urliste
     cof1 = args.cof1
     cof2 = args.cof2
     plot = args.plot
+    n = args.n
+    k = args.k
+    rep = args.rep
+    no_rep = args.no_rep
+    comb = args.comb
+    var = args.var
+    per = args.per
 
-    urliste_uebung1 = Uebungsblatt1(urliste)
+    """
+           Characterization of samples
+    """
+    if cof1 and cof2:
+        urliste_uebung1 = Uebungsblatt1(urliste)
+        urliste_uebung1.calculate_coefficient(cof1, cof2)
     if urliste:
+        urliste_uebung1 = Uebungsblatt1(urliste)
         urliste_uebung1.characteristics_of_sample()
     if plot == True:
+        urliste_uebung1 = Uebungsblatt1(urliste)
         urliste_uebung1.plot_diagramms()
+
+    """
+        Korrelationskoeffizienten
+    """
     if cof1 and cof2:
+        urliste_uebung1 = Uebungsblatt1(urliste)
         urliste_uebung1.calculate_coefficient(cof1,cof2)
+
+    """
+        Variation, Combination and permutation
+    """
+    if n and k:
+        if comb:
+            if rep:
+                uebungsblatt2 = Uebungsblatt2(n, k, repetition=rep)
+            else:
+                uebungsblatt2 = Uebungsblatt2(n, k, repetition=no_rep)
+            uebungsblatt2.combinatoric()
+        if var:
+            if rep:
+                uebungsblatt2 = Uebungsblatt2(n, k, repetition=rep)
+            else:
+                uebungsblatt2 = Uebungsblatt2(n, k, repetition=no_rep)
+            uebungsblatt2.variation()
+        if per:
+            uebungsblatt2 = Uebungsblatt2(n, 0, repetition=rep)
+            uebungsblatt2.permutation()
+
